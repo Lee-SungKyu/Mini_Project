227cad9719a52a21b1f40460c8581378a6a29105
SK <nicky7164@gmail.com>
2021-04-17 03:20:31 +0900

    finl product.h

--- product.h
+++ product.h
@@ -2,16 +2,16 @@
 #include <stdlib.h>
 
 typedef struct{
-    char name[100]; //재품이름
-    int quantity; //재품량
-    int price; // 재품가격
-    int star_rate; // 재품 rating
-    int star_count; // 재품 rating 개수
+    char name[100]; //제품이름
+    int quantity; //제품량
+    int price; // 제품가격
+    int star_rate; // 제품 rating
+    int star_count; // 제품 rating 개수
 }Shopping;
 
 
-int addProduct(Shopping *p); //재품 추가
-void readProduct(Shopping p); //하나의 재품 출력
-int updateProduct(Shopping *p); //재품 수정
-void deleteProduct(Shopping *p); //재품 삭제
+int addProduct(Shopping *p); //제품 추가
+void readProduct(Shopping p); //하나의 제품 출력
+int updateProduct(Shopping *p); //제품 수정
+void deleteProduct(Shopping *p); //제품 삭제
 
